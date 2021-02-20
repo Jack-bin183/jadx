8f90f9c208bf3b67fdc1872076cc25a0e2164417
Skylot <skylot@gmail.com>
2021-02-20 19:27:53 +0000

    feat(gui): add code comments (#359)

--- jadx-core/src/main/java/jadx/core/dex/nodes/ClassNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/ClassNode.java
@@ -371,6 +371,15 @@ public class ClassNode extends NotificationAttrNode implements ILoadable, ICodeN
 		return null;
 	}
 
+	public FieldNode searchFieldByShortId(String shortId) {
+		for (FieldNode f : fields) {
+			if (f.getFieldInfo().getShortId().equals(shortId)) {
+				return f;
+			}
+		}
+		return null;
+	}
+
 	public MethodNode searchMethod(MethodInfo mth) {
 		return mthInfoMap.get(mth);
 	}
